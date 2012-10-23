1bd8f80860bf160b0ba1e0f326b624106c28355b
Jean-Philippe Camguilhem <jp.camguilhem@gmail.com>
2012-10-23 08:41:29 +0200

    create egg

---
+++ setup.py
@@ -0,0 +1,50 @@
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+    CONTRIBUTORS = open(os.path.join(here, 'CONTRIBUTORS.txt')).read()
+except IOError:
+    README = CHANGES = CONTRIBUTORS = ''
+
+long_description = README + '\n\n'+ CHANGES + '\n\n'+ CONTRIBUTORS
+
+install_requires=['setuptools', 'pynagios >= 0.1.1']
+
+if sys.version_info[:2] < (2, 7):
+    install_requires.append('argparse')
+
+setup(name='paulla.checkplanetdiff',
+      version=version,
+      description=('A nagios|icinga plugin to check diff delay '
+                   'with the official OpenStreetMap Planet.'
+                   ),
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='Nagios Icinga OSM',
+      author='Jean-Philippe Camguilhem',
+      author_email='jp.camguilhem+eggs@gmail.com',
+      url='http://www.paulla.asso.fr',
+      license='bsd',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['paulla'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points="""
+      [console_scripts]
+      check_planetdiff = paulla.check_planetdiff.check:run
+      """,
+      )
---
+++ src/paulla/checkplanetdiff/check.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Doc here.
+"""
+
+__docformat__ = 'restructuredtext en'
+
+def run():
+    """Run check."""
+    print 'something'
+
+# vim:set et sts=4 ts=4 tw=80:
